80d5f743508aba53105a0726ea525a04800bc43d
SmejkalJakub <smejkal.jakub99@gmail.com>
2022-09-20 13:33:03 +0000

    Deploying to gh-pages from @ SmejkalJakub/twr-sdk@9c459ff98006e02ffa6f826a450db64ff82ac7c7 🚀

---
+++ navtreedata.js
@@ -0,0 +1,57 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Firmware SDK", "index.html", [
+    [ "Overview", "index.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"group__twr__data__stream.html#ga68751dfe67e773062503f4b9c2b0a07b",
+"group__twr__gfx.html#ga92386f089805366534bf5fd5cc7e2d8b",
+"group__twr__module__climate.html#gaa2360d9f20f8731bec68ddb26f8d24fe",
+"group__twr__mpl3115a2.html#gad5c84e2f09fc71192ae868db9bc00a0d",
+"group__twr__sgp30.html#ga3610639ec6e906015a9c7869b1e2fcdf",
+"group__twr__tag__temperature.html#ga994d04b114b7556ed73cd903e60e80fc",
+"structtwr__dma__channel__config__t.html#a100aa084cf35ebcf1e1a61a07b725443",
+"structtwr__timer__irq__t.html#ae87ed1d2f0cc002039020a4bb34275ac",
+"unionRTC__DR.html#ad5a6570ae98d510ed345c0826c06a840"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
